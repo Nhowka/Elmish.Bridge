fe36a590455c88928f590e87b9896ce8e989670c
Nhowka <menhowca@gmail.com>
2024-07-25 22:18:47 -0300

    State for version 7.0.1 of the server specific implementation

--- src/Giraffe/Library.fs
+++ src/Giraffe/Library.fs
@@ -12,59 +12,80 @@ module Giraffe =
     /// Giraffe's server used by `ServerProgram.runServerAtWith` and `ServerProgram.runServerAt`
     /// Creates a `HttpHandler`
     let server endpoint inboxCreator : HttpHandler =
-        let ws (next : HttpFunc) (ctx : HttpContext) =
+        let ws (next: HttpFunc) (ctx: HttpContext) =
             task {
                 if ctx.WebSockets.IsWebSocketRequest then
                     let! webSocket = ctx.WebSockets.AcceptWebSocketAsync()
-                    let (sender,closer) =
-                        inboxCreator
-                            (fun (s:string) ->
-                            let resp =
-                                s
-                                |> System.Text.Encoding.UTF8.GetBytes
-                                |> ArraySegment
+
+                    let (sender, closer) =
+                        inboxCreator (fun (s: string) ->
+                            let resp = s |> System.Text.Encoding.UTF8.GetBytes |> ArraySegment
+
                             task {
-                              if webSocket.State = WebSocketState.Open || webSocket.State = WebSocketState.CloseReceived then
-                                do! webSocket.SendAsync
-                                     (resp, WebSocketMessageType.Text, true,
-                                       CancellationToken.None)} |> Async.AwaitTask)
+                                if
+                                    webSocket.State = WebSocketState.Open
+                                    || webSocket.State = WebSocketState.CloseReceived
+                                then
+                                    do!
+                                        webSocket.SendAsync(
+                                            resp,
+                                            WebSocketMessageType.Text,
+                                            true,
+                                            CancellationToken.None
+                                        )
+                            }
+                            |> Async.AwaitTask)
+
                     let skt =
                         task {
                             let buffer = Array.zeroCreate 4096
                             let mutable loop = true
                             let mutable frame = []
+
                             while loop do
-                                let! msg = webSocket.ReceiveAsync
-                                               (ArraySegment(buffer),
-                                                CancellationToken.None)
-                                match msg.MessageType,
-                                      buffer.[0..msg.Count - 1],
-                                      msg.EndOfMessage, msg.CloseStatus with
+                                let! msg = webSocket.ReceiveAsync(ArraySegment(buffer), CancellationToken.None)
+
+                                match
+                                    msg.MessageType, buffer.[0 .. msg.Count - 1], msg.EndOfMessage, msg.CloseStatus
+                                with
                                 | _, _, _, s when s.HasValue ->
-                                    do! webSocket.CloseOutputAsync
-                                            (WebSocketCloseStatus.NormalClosure,
-                                             null, CancellationToken.None)
+                                    do!
+                                        webSocket.CloseOutputAsync(
+                                            WebSocketCloseStatus.NormalClosure,
+                                            null,
+                                            CancellationToken.None
+                                        )
+
                                     loop <- false
-                                | WebSocketMessageType.Text, data, complete,
-                                  _ ->
-                                    frame <- data :: frame
+                                | WebSocketMessageType.Text, data, complete, _ ->
+
                                     if complete then
-                                        frame
-                                        |> List.rev
-                                        |> Array.concat
+                                        match frame with
+                                        | [] -> data
+                                        | xs ->
+                                            frame <- []
+                                            (data :: xs) |> List.rev |> Array.concat
+
                                         |> System.Text.Encoding.UTF8.GetString
                                         |> sender
-                                        frame <- []
+                                    else
+                                        frame <- data :: frame
                                 | _ -> ()
                         }
+
                     try
                         do! skt
-                    with _ -> ()
-                    closer()
+                    with _ ->
+                        ()
+
+                    closer ()
                     return Some ctx
-                else return None
+                else
+                    return None
             }
+
         route endpoint >=> ws
 
     /// Prepare app to use websockets
-    let useWebSockets (app : IApplicationBuilder) = app.UseWebSockets(WebSocketOptions(KeepAliveInterval=TimeSpan.FromSeconds 30.))
+    let useWebSockets (app: IApplicationBuilder) =
+        app.UseWebSockets(WebSocketOptions(KeepAliveInterval = TimeSpan.FromSeconds 30.))
--- src/Suave/Library.fs
+++ src/Suave/Library.fs
@@ -11,38 +11,35 @@ module Suave =
 
     /// Suave's server used by `ServerProgram.runServerAtWith` and `ServerProgram.runServerAt`
     /// Creates a `WebPart`
-    let server endpoint inboxCreator : WebPart=
-        let ws (webSocket : WebSocket) _ =
-            let (sender,closer) =
-                inboxCreator (fun (s : string) ->
-                    let resp =
-                        s
-                        |> System.Text.Encoding.UTF8.GetBytes
-                        |> ByteSegment
+    let server endpoint inboxCreator : WebPart =
+        let ws (webSocket: WebSocket) _ =
+            let (sender, closer) =
+                inboxCreator (fun (s: string) ->
+                    let resp = s |> System.Text.Encoding.UTF8.GetBytes |> ByteSegment
                     webSocket.send Text resp true |> Async.Ignore)
 
             let skt =
                 socket {
                     let mutable loop = true
                     let mutable buffer = []
-                    while loop do
-                        let! msg = webSocket.read()
-                        match msg with
-                        | (Continuation | Text), data, complete 
-                            if complete then
-                                let completeData =
-                                    match buffer with
-                                    | [] -> data
-                                    | xs ->
-                                        buffer <- []
-                                        (data :: xs) |> List.rev |> Array.concat
 
-                                completeData
+                    while loop do
+                        let! msg = webSocket.read ()
+
+                        match msg with
+                        | (Continuation | Text), data, complete ->
+                            if complete then
+                                match buffer with
+                                | [] -> data
+                                | xs ->
+                                    buffer <- []
+                                    (data :: xs) |> List.rev |> Array.concat
+
                                 |> UTF8.toString
                                 |> sender
                             else
                                 buffer <- data :: buffer
-                                
+
                         | (Close, _, _) ->
                             let emptyResponse = [||] |> ByteSegment
                             do! webSocket.send Close emptyResponse true
@@ -52,7 +49,8 @@ module Suave =
 
             async {
                 let! result = skt
-                closer()
+                closer ()
                 return result
             }
+
         path endpoint >=> handShake ws
